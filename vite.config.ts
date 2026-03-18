c56d9af7aa59d3e45f89b001a2f66663dcc148d9
Rui Sasaki <ruisachusasaki@users.noreply.github.com>
2026-03-18 00:04:14 +0000

    checkpoint: pre-session 2026-03-18 00:04:14

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     react(),
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+    process.env.REPL_ID !== undefined &&
+    process.env.REPL_SLUG !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
             m.cartographer(),
